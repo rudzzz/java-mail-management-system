9986039e8c36febd9ce6140c73aeddc4b8bbfdc2
rudzzz <rudsolza13@gmail.com>
2022-10-09 09:21:16 -0300

    constructor and variables added

--- src/Mail.java
+++ src/Mail.java
@@ -1,4 +1,14 @@
 
 public class Mail {
-
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private String alternateEmail;
+	
+	public Mail(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("Email created: " + this.firstName + "." + this.lastName);
+	}
 }
