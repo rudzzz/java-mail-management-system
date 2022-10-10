bda1c19584cbf2b104f5d8fc4d989c2d5afda933
rudzzz <rudsolza13@gmail.com>
2022-10-10 19:49:50 -0300

    method to ask the employees department added

--- src/Mail.java
+++ src/Mail.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Mail {
 	private String firstName;
@@ -10,5 +11,34 @@ public class Mail {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		System.out.println("Email created: " + this.firstName + "." + this.lastName);
+		
+		//method to ask the employees department
+		this.department = Department(); 
 	}
+	
+	private String Department() {
+		System.out.println("What is the deparament?");
+		System.out.println("1 - Development\n2 - Sales\n3 - Accounting");
+		Scanner scanner = new Scanner(System.in);
+		String choice = scanner.next();
+		
+		switch(choice) {
+			case "1":
+				System.out.println("Development"); 
+				break;
+			case "2":
+				System.out.println("Sales"); 
+				break;
+			case "3":
+			System.out.println("Accounting"); 
+				break;
+			default:
+				System.out.println("Invalid Command!"); 
+				break;
+		}
+		scanner.close();
+		return choice;
+	}
+
+
 }
