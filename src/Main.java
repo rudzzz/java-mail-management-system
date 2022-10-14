e42dbaa8ab03e7a5212b30d4d09c6eb38ff8c177
rudzzz <rudsolza13@gmail.com>
2022-10-14 12:18:41 -0300

    changed the password

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,10 @@ public class Main {
 		Mail email = new Mail("Acir", "Rudson");
 		
 		email.setAlternateEmail("rudson@acir.com");
+		email.changePassword("abcDE$123");
 		System.out.println("---------------------");
 		System.out.println("New email: " + email.getalternateEmail());;
+		System.out.println("New password: " + email.getPassword());
 	}
 
 }
