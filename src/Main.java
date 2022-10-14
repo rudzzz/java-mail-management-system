b2457e366bef56805a19c42b7d35fafe650271e5
rudzzz <rudsolza13@gmail.com>
2022-10-13 21:00:18 -0300

    alternate email generated

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		Mail email = new Mail("Acir", "Rudson");
+		
+		email.setAlternateEmail("rudson@acir.com");
+		System.out.println("---------------------");
+		System.out.println("New email: " + email.getalternateEmail());;
 	}
 
 }
